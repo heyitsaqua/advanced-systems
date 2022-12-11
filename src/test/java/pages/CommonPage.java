3b0988293bc3bdad01c5cc2921d17b40974f632c
Nurik <nicksbastion@yahoo.com>
2022-12-11 16:55:52 -0500

    Nurik's user stories

--- src/test/java/pages/CommonPage.java
+++ src/test/java/pages/CommonPage.java
@@ -7,4 +7,7 @@ public interface CommonPage {
     String XPATH_TEMPLATE_TEXT_CONTAINS = "//*[contains(text(), '%s')]";
     String XPATH_TEMPLATE_INPUT_FIELD = "//input[@placeholder='%s']";
     String XPATH_TEMPLATE_INPUT_FIELD2 = "//input[@name='%s']";
+    String XPATH_TEMPLATE_TEXT2 = "//div[@class='menu-bg']//a[contains(@href,'%s')]";
+    String XPATH_TEMPLATE_TEXT3 = "//li[text()='Stay Connected:']//following-sibling::li/a/i[@class='fa fa-twitter-square']";
+    String XPATH_TEMPLATE_TEXT4 = "//*[@class='']";
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,10 +5,31 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.BrowserUtils;
 
+import java.util.List;
+
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(BrowserUtils.getDriver(), this);
     }
 
+    @FindBy(xpath = "//li[text()='Stay Connected:']//following-sibling::li")
+    public List<WebElement> mediaBtns2;
 
+    @FindBy(xpath = "//li[text()='Stay Connected:']//following-sibling::li")
+    public WebElement mediaBtns1;
+
+    @FindBy(xpath = "//div[@class='client-testimonial-carousel owl-carousel owl-theme owl-loaded owl-drag']//following-sibling::div/div[@class='text']")
+    public List<WebElement> peopleNames;
+
+    @FindBy(xpath = "//i[@class='fa fa-facebook-square']")
+    public WebElement facebookBtn;
+
+    @FindBy(xpath = "//i[@class='fa fa-twitter-square']")
+    public WebElement twitterBtn;
+
+    @FindBy(xpath = "//i[@class='fa fa-google-plus-square']")
+    public WebElement googleBtn;
+
+    @FindBy(xpath = "//i[@class='fa fa-linkedin-square']")
+    public WebElement linkedinBtn;
 }
