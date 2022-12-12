c153342932c5b7d390497d3f301f28abf16d8b2c
@OksanaSkrynnik <oksanaskrynnik@gmail.com>
2022-12-11 21:33:52 -0500

    practicing git Oksana

--- src/test/java/pages/GitPractice_ignore.java
+++ src/test/java/pages/GitPractice_ignore.java
@@ -3,5 +3,6 @@ package pages;
 public class GitPractice_ignore {
     public static void main(String[] args) {
         System.out.println("First Git Commit");
+        System.out.println("Oksana is trying to practice Git");
     }
 }
